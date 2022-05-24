05a62a3e41bc0557cbef532c43b06062c26365d3
Jinay Jain <jinaybjain@gmail.com>
2022-05-24 12:25:59 -0400

    Simplify ray intersection code and shade surface normals

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,16 @@ use bounce::{
 };
 
 fn ray_color(r: Ray) -> Color {
-    if hit_sphere(Point::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::new(1.0, 0.0, 0.0);
+
+    let sphere_center = Point::new(0.0, 0.0, -1.0);
+    let t = hit_sphere(sphere_center, 0.5, r);
+
+    if t > 0.0 {
+        // compute normal vector from sphere center to hit location
+        let normal: Vec3<f64> = (r.at(t) - sphere_center).into();
+        let normal = normal.unit();
+
+        return 0.5 * Color::new(normal.x() + 1.0, normal.y() + 1.0, normal.z() + 1.0);
     }
 
     let unit = r.direction().unit();
@@ -14,35 +22,48 @@ fn ray_color(r: Ray) -> Color {
     Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
 }
 
-fn hit_sphere(center: Point<Double>, radius: Double, r: Ray) -> bool {
+/// Checks whether a given ray will hit the a sphere of a given radius and center
+fn hit_sphere(center: Point<Double>, radius: Double, r: Ray) -> f64 {
     // t^2 b . b + 2tb . (A - C) + (A - C) . (A - C) - r^2 = 0
     // A = origin
     // b = direction
+    // C = circle center
+    // r = radius
 
-    let dir = r.direction();
-    let orig = r.origin();
+    let direction = r.direction();
+    let ray_origin = r.origin();
 
-    let offset = Vec3::from(orig - center);
+    let offset = Vec3::from(ray_origin - center);
 
-    // Form quadratic for sphere
-    let a = dir.dot(dir);
-    let b = 2.0 * (dir.dot(offset));
-    let c = offset.dot(offset) - radius.powi(2);
+    // Form quadratic for sphere intersection checking (simplified)
+    let a = direction.len().powi(2);
+    let half_b = direction.dot(offset);
+    let c = offset.len().powi(2) - radius.powi(2);
 
-    let det = b.powi(2) - 4.0 * a * c;
+    let discriminant = half_b.powi(2) - a * c;
 
-    det > 0.0
+    if discriminant < 0.0 {
+        // unable to find a hit
+        -1.0
+    } else {
+        // return the full solution to the quadratic
+        (-half_b - discriminant.sqrt()) / a
+    }
 }
 
 fn main() {
+    
+    // define image parameters
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
     let image_height = (image_width as f64 / aspect_ratio) as u32;
 
+    // define the parameters of the image plane in world space
     let viewport_height = 2.0;
     let viewport_width = aspect_ratio * viewport_height;
     let focal_length = 1.0;
 
+    // compute useful points and vectors about the world space / image plane
     let origin = Point::new(0.0, 0.0, 0.0);
     let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
     let vertical = Vec3::new(0.0, viewport_height, 0.0);
@@ -51,6 +72,8 @@ fn main() {
         - (vertical / 2.0).into()
         - Vec3::new(0.0, 0.0, focal_length).into();
 
+    
+    // Required metadata at the beginning of PPM files
     println!("P3");
     println!("{} {}", image_width, image_height);
     println!("255");
