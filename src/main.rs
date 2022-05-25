cbd1bd0971a66d5648cd6f021fa9eaa18c42f39f
Jinay Jain <jinaybjain@gmail.com>
2022-05-24 23:03:07 -0400

    Add anti-aliasing using many samples per pixel

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,7 @@ fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
     let image_height = (image_width as f64 / aspect_ratio) as u32;
+    let samples_per_pixel = 100;
 
     // Camera
     let camera = Camera::default();
@@ -42,15 +43,27 @@ fn main() {
 
     for j in (0..image_height).rev() {
         for i in 0..image_width {
-            let u = (i as f64) / (image_width - 1) as f64;
-            let v = (j as f64) / (image_height - 1) as f64;
+            let mut pixel_color = Color::new(0.0, 0.0, 0.0);
 
-            let r = camera.ray_at(u, v);
+            for _ in 0..samples_per_pixel {
+                let u = (i as f64 + rand::random::<f64>()) / (image_width - 1) as f64;
+                let v = (j as f64 + rand::random::<f64>()) / (image_height - 1) as f64;
 
-            // TODO: fix clamped add
-            let c = ray_color(r, &world);
+                let r = camera.ray_at(u, v);
 
-            println!("{}", c);
+                // TODO: fix clamped add
+                pixel_color += ray_color(r, &world);
+            }
+
+            let scale = 1.0 / (samples_per_pixel as f64);
+
+            let final_color = Color::new(
+                pixel_color.r() * scale,
+                pixel_color.g() * scale,
+                pixel_color.b() * scale,
+            );
+
+            println!("{}", final_color);
         }
     }
 }
